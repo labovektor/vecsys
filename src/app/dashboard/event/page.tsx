9d8f2a5eb4243556c33e9f17cabf5e453f779b8a
Anjar Dwi Hariadi <51255126+anjarmath@users.noreply.github.com>
2025-04-17 18:50:56 +0700

    Kelola event get create (#8)
    * feat: get events + view as table
    
    * feat: add new event
    
    * fix: loading state

---
+++ src/app/dashboard/event/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import handleRequest from "@/axios/request";
+import { DataTable } from "@/components/table/data-table";
+import { eventColumn } from "@/features/event/components/event-column";
+import NewEventDialog from "@/features/event/components/new-event";
+import { Event } from "@/features/event/dto";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { toast } from "sonner";
+
+const EventPage = () => {
+  const { data: events } = useQuery({
+    queryKey: ["events"],
+    queryFn: async () =>
+      handleRequest<Event[]>("GET", "/admin/event").then((res) => {
+        if (res.error) {
+          toast.error(res.error.message);
+        }
+        return res.data;
+      }),
+  });
+  return (
+    <DataTable
+      data={events || []}
+      columns={eventColumn}
+      actions={<NewEventDialog />}
+    />
+  );
+};
+
+export default EventPage;
