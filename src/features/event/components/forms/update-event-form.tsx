3981425be76f0ea22a0d525ee00c181aa091653a
anjarmath <anjarmath@gmail.com>
2025-05-01 17:20:41 +0700

    optimize tab & informasi umum tab

--- src/features/event/components/forms/update-event-form.tsx
+++ src/features/event/components/forms/update-event-form.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import React, { useRef } from 'react';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useRouter } from 'next/navigation';
-import { toast } from 'sonner';
-import Image from 'next/image';
-import handleRequest from '@/axios/request';
-import { Button } from '@/components/ui/button';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import handleRequest from "@/axios/request";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -15,11 +14,12 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { Event } from '../../dto';
-import { UpdateEventSchemaType, updateEventSchema } from '../../schema';
-import { getQueryClient } from '@/lib/get-query-client';
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Event } from "../../dto";
+import { UpdateEventSchemaType, updateEventSchema } from "../../schema";
+import { createFormData } from "@/lib/utils";
+import { CircleOff } from "lucide-react";
 
 interface UpdateEventFormProps {
   eventId: string;
@@ -28,67 +28,37 @@ interface UpdateEventFormProps {
 
 const UpdateEventForm = ({ eventId, event }: UpdateEventFormProps) => {
   const router = useRouter();
-  const queryClient = getQueryClient();
-  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [preview, setPreview] = useState<string | undefined>(
+    event.icon && `${process.env.NEXT_PUBLIC_API_URL}${event.icon}`
+  );
 
   const form = useForm<UpdateEventSchemaType>({
     resolver: zodResolver(updateEventSchema),
     defaultValues: {
-      name: event.name || "",
-      desc: event.desc || "",
-      group_member_num: event.group_member_num || 0,
-      participant_target: event.participant_target || 0,
-      period: event.period || "",
+      name: event.name ?? "",
+      desc: event.desc ?? "",
+      group_member_num: event.group_member_num ?? 3,
+      participant_target: event.participant_target ?? 0,
+      period: event.period ?? "",
+      icon: null,
     },
   });
 
   async function onSubmit(values: UpdateEventSchemaType) {
-    try {
+    const data = createFormData(values);
 
-      const formData = new FormData();
-      if (values.name) {
-        formData.append("name", values.name);
-      }
-      if (values.desc) {
-        formData.append("desc", values.desc);
-      }
-      if (values.group_member_num) {
-        formData.append("group_member_num", values.group_member_num.toString());
-      }
-      if (values.participant_target) {
-        formData.append("participant_target", values.participant_target.toString());
-      }
-      if (values.period) {
-        formData.append("period", values.period);
-      }
+    const res = await handleRequest<unknown>(
+      "PATCH",
+      `/admin/event/${eventId}`,
+      data
+    );
 
-      const iconFile = fileInputRef.current?.files?.[0];
-      if (iconFile) {
-        formData.append("icon", iconFile);
-      } const { error } = await handleRequest<unknown>(
-        "PATCH",
-        `/admin/event/${eventId}`,
-        formData
-      );
-
-      if (error) {
-        toast.error(error.message);
-        return;
-      }
-
-      toast.success("Event updated successfully");
-
-      queryClient.invalidateQueries({ queryKey: ["event"] });
-
-      const newEventName = values.name || event.name;
-      router.push(`/dashboard/event/${eventId}?event_name=${newEventName}`);
-
-      router.refresh();
-
-    } catch (error) {
-      console.error(error);
-      toast.error("An error occurred while updating the event");
-    } 
+    if (res.error) {
+      toast.error(res.error.message);
+      return;
+    }
+    toast.success("Event updated");
+    router.push(`/dashboard/event/${eventId}?event_name=${values.name}`);
   }
 
   return (
@@ -96,11 +66,12 @@ const UpdateEventForm = ({ eventId, event }: UpdateEventFormProps) => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <FormField
           control={form.control}
-          name="name" render={({ field }) => (
+          name="name"
+          render={({ field }) => (
             <FormItem>
               <FormLabel>Nama Event</FormLabel>
               <FormControl>
-                <Input {...field} placeholder='Nama Event'/>
+                <Input {...field} placeholder="Nama Event" />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -109,11 +80,12 @@ const UpdateEventForm = ({ eventId, event }: UpdateEventFormProps) => {
 
         <FormField
           control={form.control}
-          name="desc" render={({ field }) => (
+          name="desc"
+          render={({ field }) => (
             <FormItem>
               <FormLabel>Deskripsi Event</FormLabel>
               <FormControl>
-                <Input {...field} placeholder='Deskripsi Event'/>
+                <Input {...field} placeholder="Deskripsi Event" />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -122,49 +94,64 @@ const UpdateEventForm = ({ eventId, event }: UpdateEventFormProps) => {
 
         <FormField
           control={form.control}
-          name="group_member_num" render={({ field }) => (
+          name="group_member_num"
+          render={({ field }) => (
             <FormItem>
               <FormLabel>Jumlah Beregu</FormLabel>
               <FormControl>
-                <Input type="number" {...field} placeholder='Jumlah Beregu'/>
+                <Input type="number" {...field} placeholder="Jumlah Beregu" />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
-        />        
-        <div>
-          <FormLabel htmlFor="icon">Icon</FormLabel>
-          <Input
-            id="icon"
+        />
+        <div className=" space-y-2">
+          <FormField
+            control={form.control}
             name="icon"
-            type="file"
-            ref={fileInputRef}
-            accept="image/*"
-          />          
-          <div className="mt-2">
-            {event.icon ? (
-              <div className="flex flex-col items-start gap-2">
-                <Image
-                  src={`${process.env.NEXT_PUBLIC_API_URL}${event.icon}`} 
-                  alt={`${event.name} icon`}
-                  width={64}
-                  height={64}
-                  className="h-16 w-16 object-contain rounded border border-gray-200"
-                />
+            render={({ field: { value, ...fieldValues } }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    accept="image/*"
+                    type="file"
+                    {...fieldValues}
+                    onChange={async (e) => {
+                      if (!e.target.files) {
+                        return;
+                      }
+                      const file = e.target.files[0];
+
+                      fieldValues.onChange(file);
+                      setPreview(URL.createObjectURL(file));
+                    }}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div>
+            {preview ? (
+              <div className=" w-10 h-10 rounded-full flex items-center justify-center overflow-hidden">
+                <img src={preview} className=" object-contain" />
               </div>
             ) : (
-              <p className="text-sm text-gray-500">No icon set</p>
+              <CircleOff className=" text-muted-foreground" />
             )}
           </div>
         </div>
 
         <FormField
           control={form.control}
-          name="participant_target" render={({ field }) => (
+          name="participant_target"
+          render={({ field }) => (
             <FormItem>
               <FormLabel>Target Peserta</FormLabel>
               <FormControl>
-                <Input type="number" {...field} placeholder='Target Peserta'/>
+                <Input type="number" {...field} placeholder="Target Peserta" />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -173,24 +160,25 @@ const UpdateEventForm = ({ eventId, event }: UpdateEventFormProps) => {
 
         <FormField
           control={form.control}
-          name="period" render={({ field }) => (
+          name="period"
+          render={({ field }) => (
             <FormItem>
               <FormLabel>Periode Event</FormLabel>
               <FormControl>
-                <Input {...field} placeholder='Priode Event'/>
+                <Input {...field} placeholder="Priode Event" />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-        <div className='flex justify-end w-full'>
-        <Button
-          type="submit"
-          className=""
-          disabled={form.formState.isSubmitting}
-        >
+        <div className="flex justify-end w-full">
+          <Button
+            type="submit"
+            className=""
+            disabled={form.formState.isSubmitting}
+          >
             {form.formState.isSubmitting ? "Menyimpan..." : "Simpan Perubahan"}
-        </Button>
+          </Button>
         </div>
       </form>
     </Form>
--- src/features/event/components/tabs/informasi-umum.tsx
+++ src/features/event/components/tabs/informasi-umum.tsx
@@ -1,16 +1,15 @@
-import handleRequest from '@/axios/request';
-import { Card } from '@/components/ui/card';
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { toast } from 'sonner';
-import ToggleEventStatus from '../toggle-event-status';
-import { Event } from '../../dto';
-import UpdateEventForm from '../forms/update-event-form';
+import handleRequest from "@/axios/request";
+import { Card } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { toast } from "sonner";
+import ToggleEventStatus from "../toggle-event-status";
+import { Event } from "../../dto";
+import UpdateEventForm from "../forms/update-event-form";
 
 const InformasiUmum = ({ id }: { id: string }) => {
-
   const { data: event, isLoading } = useQuery({
-    queryKey: ["event"],
+    queryKey: [id],
     queryFn: async () =>
       handleRequest<Event>("GET", `/event/${id}`).then((res) => {
         if (res.error) {
@@ -19,12 +18,15 @@ const InformasiUmum = ({ id }: { id: string }) => {
         return res.data;
       }),
   });
-  const isActive = event?.active ?? false;
 
   return (
     <Card className="w-full h-full p-4 bg-white border border-gray-200 shadow-sm rounded-md">
-      <div className='w-full flex justify-end mb-4'>
-        <ToggleEventStatus eventId={id} isActive={isActive} />
+      <div className="w-full flex justify-end mb-4">
+        <ToggleEventStatus
+          key={`${event?.active}`}
+          eventId={id}
+          isActive={event?.active ?? false}
+        />
       </div>
 
       {isLoading ? (
@@ -32,10 +34,12 @@ const InformasiUmum = ({ id }: { id: string }) => {
       ) : event ? (
         <UpdateEventForm eventId={id} event={event} />
       ) : (
-        <div className="flex justify-center py-8">Failed to load event data</div>
+        <div className="flex justify-center py-8">
+          Failed to load event data
+        </div>
       )}
     </Card>
   );
-}
+};
 
 export default InformasiUmum;
