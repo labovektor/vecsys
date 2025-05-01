6505a23c90febf555936ead50369c67a3c6e5bed
Alvalen Shafel <alvalen.shafel04@gmail.com>
2025-05-01 16:17:44 +0700

    Add event update form with validation (#12)
    * feat: add update event form and schema validation
    
    * chore: configure image remote patterns for external API
    
    * feat: translate event update form labels and placeholders to Indonesian
    
    * feat: update event form to handle new event name
    
    * chore: remove unnecessary console log and clean up code
    
    * feat: change is submit state
    
    * feat: update request handling and refine event schema validation
    
    * fix: remove unused state

--- src/features/event/schema.ts
+++ src/features/event/schema.ts
@@ -1,7 +1,24 @@
 import { z } from "zod";
 
 export const newEventSchema = z.object({
-  name: z.string().min(2),
+	name: z.string().min(2),
 });
 
 export type NewEventSchemaType = z.infer<typeof newEventSchema>;
+
+export const updateEventSchema = z.object({
+	name: z.string(),
+	desc: z
+		.string(),
+	group_member_num: z.coerce
+		.number()
+		.int()
+		.positive("Must be a positive number"),
+	participant_target: z.coerce
+		.number()
+		.int()
+		.positive("Must be a positive number"),
+	period: z.string(),
+});
+
+export type UpdateEventSchemaType = z.infer<typeof updateEventSchema>;
