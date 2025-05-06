f9e04cfb44547581b64a1ee389b62141caccb236
anjarmath <anjarmath@gmail.com>
2025-05-06 15:25:27 +0700

    fix: useParticipant

--- src/provider/participant-auth-provider.tsx
+++ src/provider/participant-auth-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import handleRequest from "@/axios/request";
-import { ParticipantData } from "@/features/participant-administration/dto";
+import { ParticipantAdmistration } from "@/features/participant-administration/dto";
 import { useQuery } from "@tanstack/react-query";
 import { usePathname } from "next/navigation";
 import { createContext, useEffect, useMemo, useState } from "react";
@@ -12,7 +12,7 @@ type ParticipantAuthContextProviderProps = {
 };
 
 interface ParticipantAuthContextType {
-  user?: ParticipantData | null;
+  user?: ParticipantAdmistration | null;
   loading: boolean;
   error?: string;
   logout: VoidFunction;
@@ -36,7 +36,7 @@ export default function ParticipantAuthContextProvider({
   const { data: participant, isPending } = useQuery({
     queryKey: ["participant-profile"],
     queryFn: () =>
-      handleRequest<ParticipantData>("GET", "/user/data").then((res) => {
+      handleRequest<ParticipantAdmistration>("GET", "/user").then((res) => {
         if (res.error) {
           toast.error(res.error.message);
         }
