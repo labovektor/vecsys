4f599b2b362bd0784bb91770746082f9cfc6d8d6
labovektor <labovektor@gmail.com>
2025-05-10 20:19:49 +0700

    Develop (#14) (#17)
    * dev
    
    * Kelola event get create (#8)
    
    * feat: get events + view as table
    
    * feat: add new event
    
    * fix: loading state
    
    * feat: toggle event status (#10)
    
    * Add event edit initial pages and tabs (#11)
    
    * feat: add edit functionality to event action buttons
    
    * feat: initial event edit page
    
    * feat: show event name in edit page
    
    * feat: integrate ToggleEventStatus component in InformasiUmum
    
    * Add event update form with validation (#12)
    
    * feat: add update event form and schema validation
    
    * chore: configure image remote patterns for external API
    
    * feat: translate event update form labels and placeholders to Indonesian
    
    * feat: update event form to handle new event name
    
    * chore: remove unnecessary console log and clean up code
    
    * feat: change is submit state
    
    * feat: update request handling and refine event schema validation
    
    * fix: remove unused state
    
    * Anjar/optimize informasi umum (#13)
    
    * optimize tab & informasi umum tab
    
    * fix: updated event state
    
    ---------
    
    Co-authored-by: anjarmath <anjarmath@gmail.com>
    Co-authored-by: Anjar Dwi Hariadi <51255126+anjarmath@users.noreply.github.com>
    Co-authored-by: Alvalen Shafel <alvalen.shafel04@gmail.com>

---
+++ src/features/event/components/tabs/kelola-jenjang.tsx
@@ -0,0 +1,14 @@
+import { Card } from '@/components/ui/card';
+import React from 'react';
+
+const KelolaJenjang = () => {
+  return (
+    <Card className="w-full h-full p-4 bg-white border border-gray-200 shadow-sm rounded-md">
+      <div className='w-full flex justify-content-end'>
+        ToggleEventSta
+      </div>
+    </Card>
+  );
+}
+
+export default KelolaJenjang;
