abe88dcf0541382d42bd1d1bd2f0f2764679f9cf
Alvalen <alvalen.shafel04@gmail.com>
2025-06-15 16:14:26 +0700

    fix: wrap ChooseEvent and TabsIndex in Suspense to avoid build error

--- src/app/dashboard/participant/page.tsx
+++ src/app/dashboard/participant/page.tsx
@@ -1,6 +1,6 @@
 import TabsIndex from '@/features/participant/components/tabs';
 import ChooseEvent from '@/features/participant/components/choose-event';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 const ParticipantPage = () => {
   return (
@@ -8,8 +8,10 @@ const ParticipantPage = () => {
       <div className='flex items-center justify-between py-4 px-1'>
         <h1 className="text-xl font-bold">Kelola Peserta</h1>
       </div>
-      <ChooseEvent />
-      <TabsIndex />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ChooseEvent />
+        <TabsIndex />
+      </Suspense>
     </div>
   );
 }
