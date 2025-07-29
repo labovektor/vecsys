adcb1eaf923f0581eaea93072bfa09c24578f2d0
Farras Alif Daffa <farras2807@gmail.com>
2025-07-29 10:56:45 +0700

    feat: fixing participant detail

--- src/app/dashboard/participant/[id]/page.tsx
+++ src/app/dashboard/participant/[id]/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import handleRequest from "@/axios/request";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Participant } from "@/features/participant/dto";
 import ParticipantDetailForm from "@/features/participant/form/participant-detail-form";
+import { cn } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import { EyeIcon, PrinterIcon } from "lucide-react";
+import Link from "next/link";
 import React, { use } from "react";
 import { toast } from "sonner";
 
@@ -27,10 +29,17 @@ const ParticipantDetailPage = ({ params }: { params: Promise<{id: string}> }) =>
     <>
       <div className="flex justify-between py-4 px-1">
         <h1 className="text-xl font-bold">Detail Peserta</h1>
+        
         <div className="flex gap-2">
-          <Button className="bg-gray-800 text-white">
+
+          <Link 
+            href={participant?.payment ? `${process.env.NEXT_PUBLIC_API_URL}${participant?.payment?.invoice}` : `#`} 
+            className={cn(buttonVariants(), "bg-gray-800 text-white")}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
              <EyeIcon/> Lihat Bukti Pembayaran
-          </Button>
+          </Link>
           <Button className="bg-gray-800 text-white">
             <PrinterIcon/> Cetak Kartu Peserta
           </Button>
--- src/features/participant/dto.ts
+++ src/features/participant/dto.ts
@@ -1,5 +1,8 @@
 import { EventCategory } from "../event-category/dto";
 import { Institution } from "../event-instansi/dto";
+import { PaymentOption } from "../event-payment/dto";
+import { PaymentOptionSchemaType } from "../event-payment/schema";
+import { EventReferral } from "../event-referral/dto";
 import { EventRegion } from "../event-region/dto";
 import { Event } from "../event/dto";
 
@@ -20,6 +23,22 @@ export type Participant = {
   progress_step: string;
   verified_at: string;
   locked_at: string;
+  created_at: string;
+  updated_at: string;
+  payment: PaymentData | null;
+};
+
+export type PaymentData = {
+  id: string;
+  participant_id: string;
+  bank_name: string;
+  bank_account: string;
+  referal_id: string | null;
+  referal: EventReferral | null;
+  date: string;
+  invoice: string;
+  payment_option_id: string;
+  payment_option: PaymentOption | null;
   created_at: string;
   updated_at: string;
 };
